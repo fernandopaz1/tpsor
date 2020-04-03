16e9b36e244a5d4ce55e1b08fbcaf6d57f5a5962
Fernando Paz <ferpaz9292@gmail.com>
2020-04-03 10:39:24 -0300

    Agregamos filosofosChinos.c y sincronizacion.c

---
+++ filosofosChinos.c
@@ -0,0 +1,149 @@
+﻿#include <pthread.h>
+#include <semaphore.h>
+#include <stdio.h>
+#include <unistd.h> //para hacer sleep
+
+
+sem_t A;
+sem_t B;
+sem_t C;
+sem_t D;
+sem_t E;
+
+
+void do_nothing(int microseconds, char* mensaje){
+  usleep(microseconds); //dormir el thread, simula que esta haciendo alguna tarea
+  printf("\n %s \n",mensaje); 
+}
+
+static void * filosofo1() { 
+	while (1) {
+		//Estado Pensando
+		printf("\n %s \n","Soy el Filósofo 1 : Pensando");
+		do_nothing(2, "Eureka!");
+		
+		//Estado Agarrando palillo
+		sem_wait(&E);
+		printf("\n %s \n","Soy el Filósofo 1 : Agarrando palillo izquierdo");
+		sem_wait(&A);
+		printf("\n %s \n","Soy el Filósofo 1 : Agarrando palillo derecho");
+		//Estado Comiendo
+		printf("\n %s \n","Soy el Filósofo 1 : Comiendo");
+		sem_post(&E);
+		do_nothing(2, "Provecho!");
+		sem_post(&A);
+	}
+}
+
+
+static void * filosofo2() { 
+	while (1) {
+		//Estado Pensando
+		printf("\n %s \n","Soy el Filósofo 2 : Pensando");
+		do_nothing(2, "Eureka!");
+		
+		//Estado Agarrando palillo
+		sem_wait(&B);
+		printf("\n %s \n","Soy el Filósofo 2 : Agarrando palillo izquierdo");
+		sem_wait(&A);
+		printf("\n %s \n","Soy el Filósofo 2 : Agarrando palillo derecho");
+		//Estado Comiendo
+		printf("\n %s \n","Soy el Filósofo 2 : Comiendo");
+		sem_post(&A);
+		do_nothing(2, "Provecho!");
+		sem_post(&B);
+	}
+}
+
+static void * filosofo3() { 
+	while (1) {
+		//Estado Pensando
+		printf("\n %s \n","Soy el Filósofo 3 : Pensando");
+		do_nothing(2, "Eureka!");
+		
+		//Estado Agarrando palillo
+		sem_wait(&B);
+		printf("\n %s \n","Soy el Filósofo 3 : Agarrando palillo izquierdo");
+		sem_wait(&C);
+		printf("\n %s \n","Soy el Filósofo 3 : Agarrando palillo derecho");
+		//Estado Comiendo
+		printf("\n %s \n","Soy el Filósofo 3 : Comiendo");
+		sem_post(&B);
+		do_nothing(2, "Provecho!");
+		sem_post(&C);
+	}
+}
+
+static void * filosofo4() { 
+	while (1) {
+		//Estado Pensando
+		printf("\n %s \n","Soy el Filósofo 4 : Pensando");
+		do_nothing(2, "Eureka!");
+		
+		//Estado Agarrando palillo
+		sem_wait(&D);
+		printf("\n %s \n","Soy el Filósofo 4 : Agarrando palillo izquierdo");
+		sem_wait(&C);
+		printf("\n %s \n","Soy el Filósofo 4 : Agarrando palillo derecho");
+		//Estado Comiendo
+		printf("\n %s \n","Soy el Filósofo 4 : Comiendo");
+		sem_post(&C);
+		do_nothing(2, "Provecho!");
+		sem_post(&D);
+	}
+}
+
+static void * filosofo5() { 
+	while (1) {
+		//Estado Pensando
+		printf("\n %s \n","Soy el Filósofo 5 : Pensando");
+		do_nothing(2, "Eureka!");
+		
+		//Estado Agarrando palillo
+		sem_wait(&D);
+		printf("\n %s \n","Soy el Filósofo 5 : Agarrando palillo izquierdo");
+		sem_wait(&E);
+		printf("\n %s \n","Soy el Filósofo 5 : Agarrando palillo derecho");
+		//Estado Comiendo
+		printf("\n %s \n","Soy el Filósofo 5 : Comiendo");
+		sem_post(&D);
+		do_nothing(2, "Provecho!");
+		sem_post(&E);
+	}
+}
+
+int main(void)    {
+        pthread_t thread_1, thread_2, thread_3, thread_4,thread_5;
+        sem_init(&A,0,1);
+        sem_init(&B,0,1);
+	sem_init(&C,0,1);
+	sem_init(&D,0,1);
+	sem_init(&E,0,1);
+	
+
+        pthread_create(&thread_1, NULL, *filosofo1, NULL);
+        pthread_create(&thread_2, NULL, *filosofo2, NULL);
+        pthread_create(&thread_3, NULL, *filosofo3, NULL);
+	pthread_create(&thread_4, NULL, *filosofo4, NULL);
+	pthread_create(&thread_5, NULL, *filosofo5, NULL);
+
+	
+
+        pthread_join(thread_1, NULL);
+        pthread_join(thread_2, NULL);
+        pthread_join(thread_3, NULL);
+	pthread_join(thread_4, NULL);
+	pthread_join(thread_5, NULL);
+        
+	sem_destroy(&A);
+	sem_destroy(&B);
+	sem_destroy(&C);
+	sem_destroy(&D);
+	sem_destroy(&E);
+	
+        pthread_exit(NULL);
+
+        printf("\n %s \n"," ");
+    
+        return 0;
+}
---
+++ sincronizacion.c
@@ -0,0 +1,185 @@
+﻿#include <pthread.h>
+#include <semaphore.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+sem_t P1;
+sem_t terminaP1234;
+sem_t P1234;
+sem_t P2;
+sem_t P3;
+sem_t P4;
+sem_t P5;
+sem_t P6;
+sem_t terminaP6;
+sem_t P7;
+
+
+
+static void * pienso(void *iteracion) {
+	int N = *((int *) iteracion);
+	int i=0; 
+	while (i<N) {
+		//Estado Pensando
+
+		sem_wait(&P1234);
+		sem_wait(&P1);		
+		printf("\n %s \n","Pienso");
+		sem_post(&P1234);
+		sem_post(&P2);
+		sem_post(&P3);
+		sem_post(&P4);
+		sem_post(&P5);
+		
+		i++;
+		
+	}
+}
+
+static void * lavoPlatos(void *iteracion) {
+	int N = *((int *) iteracion);
+	int i=0; 
+	while (i<N) {
+	//Estado Pensando
+		sem_wait(&P1234);
+		sem_wait(&P2);
+		printf("\n %s \n","Mientras lavo los platos");
+		sem_post(&terminaP1234);
+		sem_post(&P1234);
+		
+		i++;
+	}
+}
+
+static void * limpioPisos(void *iteracion) {
+	int N = *((int *) iteracion);
+	int i=0; 
+	while (i<N) {
+	//Estado Pensando
+		sem_wait(&P1234);
+		sem_wait(&P3);
+		printf("\n %s \n","Mientras limpio los pisos");
+		sem_post(&terminaP1234);
+		sem_post(&P1234);
+
+		i++;
+	}
+}
+
+static void * riegoPlantas(void *iteracion) {
+	int N = *((int *) iteracion);
+	int i=0; 
+	while (i<N) {
+	//Estado Pensando
+		sem_wait(&P1234);
+		sem_wait(&P4);
+		printf("\n %s \n","Mientras riego las plantas");
+		sem_post(&terminaP1234);
+		sem_post(&P1234);
+
+		i++;
+	}
+}
+
+static void * existo(void *iteracion) {
+	int N = *((int *) iteracion);
+	int i=0; 
+	while (i<N) {
+	//Estado Pensando
+		sem_wait(&P5);
+		sem_wait(&terminaP1234);
+		sem_wait(&terminaP1234);
+		sem_wait(&terminaP1234);
+		printf("\n %s \n","Existo!");
+		sem_post(&P6);
+		sem_post(&P7);
+		sem_post(&P5);
+
+		i++;
+	}
+}
+
+static void * hablar(void *iteracion) {
+	int N = *((int *) iteracion);
+	int i=0; 
+	while (i<N) {
+	//Estado Pensando
+		sem_wait(&P6);
+		printf("\n %s \n","Hablar");
+		sem_post(&terminaP6);
+		i++;
+	}
+}
+
+static void * tomarDecision(void *iteracion) {
+	int N = *((int *) iteracion); 
+	int i=0; 
+	while (i<N) {
+	//Estado Pensando
+		sem_wait(&P7);
+		sem_wait(&terminaP6);
+		printf("\n %s \n","Tomar una decisión");
+		sem_post(&P1);
+
+		i++;
+	}
+}
+
+
+
+int main(int argc,char *argv[])    {
+	int *iteraciones = malloc(sizeof(*iteraciones));
+	int a;
+	sscanf(argv[1], "%d", & a);
+	
+	*iteraciones= a;
+
+        pthread_t thread_1, thread_2, thread_3, thread_4,thread_5, thread_6,thread_7;
+	sem_init(&P1,	0,1);        
+	sem_init(&P2,	0,0);
+	sem_init(&P3,	0,0);
+	sem_init(&P4,	0,0);
+	sem_init(&terminaP1234,	0,0);
+        sem_init(&P1234,0,4);
+	sem_init(&P5,	0,0);
+	sem_init(&P6,	0,0);
+	sem_init(&terminaP6,	0,0);
+	sem_init(&P7,	0,0);
+	
+
+        pthread_create(&thread_1, NULL, *pienso, iteraciones);
+        pthread_create(&thread_2, NULL, *lavoPlatos, iteraciones);
+        pthread_create(&thread_3, NULL, *limpioPisos, iteraciones);
+	pthread_create(&thread_4, NULL, *riegoPlantas, iteraciones);
+	pthread_create(&thread_5, NULL, *existo, iteraciones);
+	pthread_create(&thread_6, NULL, *hablar, iteraciones);
+	pthread_create(&thread_7, NULL, *tomarDecision, iteraciones);
+
+	
+
+        pthread_join(thread_1, NULL);
+        pthread_join(thread_2, NULL);
+        pthread_join(thread_3, NULL);
+	pthread_join(thread_4, NULL);
+	pthread_join(thread_5, NULL);
+	pthread_join(thread_6, NULL);
+	pthread_join(thread_7, NULL);
+        
+	sem_destroy(&P1);
+	sem_destroy(&P2);
+	sem_destroy(&P3);
+	sem_destroy(&P4);
+	sem_destroy(&terminaP1234);
+	sem_destroy(&terminaP6);
+	sem_destroy(&P1234);
+	sem_destroy(&P5);
+	sem_destroy(&P6);
+	sem_destroy(&P7);
+	
+        pthread_exit(NULL);
+
+        printf("\n %s \n"," ");
+    
+        return 0;
+}
